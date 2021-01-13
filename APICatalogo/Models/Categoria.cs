9fac2156a4cfec4b6601426464fb720ecd96c230
IgorTerriaga <igsantos1996@gmail.com>
2021-01-13 01:50:40 -0300

    first Migration

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models
 {
+    [Table("Categoria")]
     public class Categoria
     {
-       
-        public int  CategoriaId { get; set; }
+        [Key]
+        public int CategoriaId { get; set; }
 
+        [Required]
+        [MaxLength(80)]
         public string Nome { get; set; }
-
+        [Required]
+        [MaxLength(300)]
         public string ImagemUrl { get; set; }
 
         public ICollection<Produto> Produtos { get; set; } = new Collection<Produto>();
--- APICatalogo/Startup.cs
+++ APICatalogo/Startup.cs
@@ -1,17 +1,11 @@
 using APICatalogo.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace APICatalogo
 {
@@ -27,8 +21,9 @@ namespace APICatalogo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
-            services.AddDbContext<AppDbContext>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
